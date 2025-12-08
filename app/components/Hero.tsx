0f131e5a09cfd9d2405492d38a42414d21ee25cc
Blazej Ciepiel <u31_blacie_lbn@technischools.com>
2025-12-08 12:10:38 +0100

    fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -48,21 +48,6 @@ const Hero: React.FC = () => {
             Sprawdź ofertę
           </button>
         </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 10, 0] }}
-            transition={{ repeat: Infinity, duration: 1.5 }}
-            className="w-6 h-10 border-2 border-white/30 rounded-full flex items-start justify-center p-2"
-          >
-            <div className="w-1.5 h-3 bg-white/50 rounded-full" />
-          </motion.div>
-        </motion.div>
       </div>
     </section>
   );
